3b98642c739ee2971632b30deeab5d8bff63b614
Mitch <cavallarovendittimichele@gmail.com>
2025-07-07 16:09:25 +0200

    V1.0.1:added export in Index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-console.log('Hello, world!')
+export {useLocalStorage} from './useLocalStorage/useLocalStorage'
+export {useSessionStorage} from './useSessionStorage/useSessionStorage'
+export {usePersistentState} from './usePersistentState/usePersistentState'
+export {useStorage} from './useStorage/useStorage'
